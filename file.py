941dfb9b0ce715da2ade03cbaff0c1b28ae04fbf
Kusa-san <mohamed.musa.kh41@gmail.com>
2025-01-11 18:23:05 +0200

    lec 13

---
+++ file.py
@@ -0,0 +1,24 @@
+x = 10
+y = 10
+# z = 3.5
+# ch = 'c'
+
+# print(x + y)
+# print( x - y)
+# print(x * y)
+# print(x / y)
+
+# print(f"this is an integer {x}, and this is an integer {y}, this is a float {z}, and this is a character {ch}")
+
+if 1>2:
+    print("1 is greater than 2")
+
+if x>y:
+    print("x is greater than y")
+elif x<y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
+for i in range(10):
+    print(i)
